bb6deaf1a252b28a0979b0306d5aa0519ed8ee75
Binay <binaydas1123@gmail.com>
2025-07-30 22:25:36 +0530

    fetching uploaded files

---
+++ app/api/files/route.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/lib/prisma";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const searchParams = req.nextUrl.searchParams;
+    const queryUserId = searchParams.get("userId");
+    const parentId = searchParams.get("parentId");
+
+    console.log("searchParamss",searchParams);
+    console.log("queryUserId",queryUserId);
+
+    if (!queryUserId || queryUserId !== userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    let userFiles;
+    if (parentId) {
+      userFiles = await prisma.file.findMany({
+        where: {
+          userId,
+          parentId
+        }
+      });
+      
+    } else {
+      userFiles = await prisma.file.findMany({
+        where: {
+          userId,
+          parentId: null
+        }
+      });
+    }
+
+    return NextResponse.json(userFiles);
+  } catch (error) {
+    console.error("Error fetching files:", error);
+    return NextResponse.json({ error: "Failed to fetch files" }, { status: 500 });
+  }
+}
